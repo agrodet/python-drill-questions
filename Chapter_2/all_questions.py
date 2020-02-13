f27d383cd4cd232eced81ebdacfabc5f07951674
grodet <42261966+agrodet@users.noreply.github.com>
2020-02-13 18:39:27 +0900

    Add all_questions file

---
+++ Chapter_2/all_questions.py
@@ -0,0 +1,18 @@
+from . import Q2_01
+# from . import Q2_02
+# from . import Q2_03
+# from . import Q2_04
+# from . import Q2_05
+# from . import Q2_06
+# from . import Q2_07
+# from . import Q2_08
+# from . import Q2_09
+# from . import Q2_10
+# from . import Q2_11
+# from . import Q2_12
+# from . import Q2_13
+# from . import Q2_14
+# from . import Q2_15
+# from . import Q2_16
+# from . import Q2_17
+# from . import Q2_18
