30e04586456d0a819c7084f071c914102eba9e0a
msyk <nii@msyk.net>
2020-02-13 11:19:16 +0900

    Add some qs

---
+++ all_start_replit.py
@@ -0,0 +1,8 @@
+import Chapter_1/all_questions
+# import Chapter_2/all_questions
+# import Chapter_3/all_questions
+# import Chapter_4/all_questions
+# import Chapter_5/all_questions
+# import Chapter_6/all_questions
+# import Chapter_7/all_questions
+# import Chapter_8/all_questions
