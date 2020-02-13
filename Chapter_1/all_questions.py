ffdf4075e404c7ade889d8574d3e2d306bcdda8b
grodet <42261966+agrodet@users.noreply.github.com>
2020-02-13 15:42:48 +0900

    Rename

--- Chapter_1/all_questions.py
+++ Chapter_1/all_questions.py
@@ -1,15 +1,15 @@
-from . import q1_01
-# from . import q1_02
-# from . import q1_03
-# from . import q1_04
-# from . import q1_05
-# from . import q1_06
-# from . import q1_07
-# from . import q1_08
-# from . import q1_09
-# from . import q1_10
-# from . import q1_11
-# from . import q1_12
-# from . import q1_13
-# from . import q1_14
-# from . import q1_15
+from . import Q1_01
+# from . import Q1_02
+# from . import Q1_03
+# from . import Q1_04
+# from . import Q1_05
+# from . import Q1_06
+# from . import Q1_07
+# from . import Q1_08
+# from . import Q1_09
+# from . import Q1_10
+# from . import Q1_11
+# from . import Q1_12
+# from . import Q1_13
+# from . import Q1_14
+# from . import Q1_15
---
+++ Chapter_8/all_questions.py
@@ -0,0 +1,10 @@
+from . import Q8_01
+# from . import Q8_02
+# from . import Q8_03
+# from . import Q8_04
+# from . import Q8_05
+# from . import Q8_06
+# from . import Q8_07
+# from . import Q8_08
+# from . import Q8_09
+# from . import Q8_10
