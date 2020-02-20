acc326769c876b597bba8b815b3bd0c092a7a5c6
msyk <nii@msyk.net>
2020-02-20 14:30:14 +0900

    importer.py updated

--- Chapter_8/importer.py
+++ Chapter_8/importer.py
@@ -1,10 +1,17 @@
-from sklearn.datasets import load_iris, load_wine, load_breast_cancer
+from sklearn.datasets import load_iris
+from sklearn.datasets import load_wine
+from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
 from sklearn.decomposition import PCA
-from sklearn.feature_selection import SelectKBest, mutual_info_classif
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, KFold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
 from sklearn.manifold import MDS
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
