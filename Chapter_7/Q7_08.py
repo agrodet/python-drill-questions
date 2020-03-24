43adb092063f34ab2c6a154cf11c6233e64f2427
msyk <nii@msyk.net>
2020-03-24 19:06:30 +0900

    Modified indentation of Q7_08.py

--- Chapter_7/Q7_08.py
+++ Chapter_7/Q7_08.py
@@ -8,14 +8,14 @@ def add_all(fr, to, index):
     """①"""
 
 
-    worker = [None] * 10
-    if __name__ == '__main__':
-        with """②""" as pool:
-            for i in range(10):
-                start = i * 10_000
-                end = (i + 1) * 10_000 - 1
-                worker[i] = """③"""
-            s = 0
-            for i in range(10):
-                s += worker[i]."""④"""
-            print("{:,}".format(s))  # 4,999,950,000
+worker = [None] * 10
+if __name__ == '__main__':
+    with """②""" as pool:
+        for i in range(10):
+            start = i * 10_000
+            end = (i + 1) * 10_000 - 1
+            worker[i] = """③"""
+        s = 0
+        for i in range(10):
+            s += worker[i]."""④"""
+        print("{:,}".format(s))  # 4,999,950,000
